9fddab8aecb789a1d6dbc384b2b68c465a43ee50
Ujjawal Kumar <ujjakuma@amazon.com>
2021-12-28 12:27:09 +0530

    Created a model.

--- myApp.js
+++ myApp.js
@@ -1,8 +1,18 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const {Schema} = require("mongoose/lib/browser");
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+let personSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
