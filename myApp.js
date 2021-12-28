1b48fe918be10cc30b796ac0fb663c56b8421218
Ujjawal Kumar <ujjakuma@amazon.com>
2021-12-28 15:15:29 +0530

    Added logic for findEditThenSave

--- myApp.js
+++ myApp.js
@@ -73,8 +73,19 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, function (err, person) {
+    if(err) {
+      done(err);
+    }
+    person.favoriteFoods.push(foodToAdd);
+    person.save(function (err, updatedPerson) {
+      if(err) {
+        done(err);
+      } else {
+        done(null, updatedPerson);
+      }
+    })
+  });
 };
 
 const findAndUpdate = (personName, done) => {
