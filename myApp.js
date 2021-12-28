aefc8bb7d6b2e270339b7de5f50cfc0959f71dfa
Ujjawal Kumar <ujjakuma@amazon.com>
2021-12-28 14:23:27 +0530

    Added logic for findOneByFood.

--- myApp.js
+++ myApp.js
@@ -52,7 +52,13 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, function (err, data) {
+    if(err) {
+      done(err);
+    } else {
+      done(null, data);
+    }
+  });
 };
 
 const findPersonById = (personId, done) => {
